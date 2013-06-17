9cf7d3075663c4b3f145be8b51877b7e19ba9ffb
Chris McGuire <chris.r.mcguire@gmail.com>
2013-06-17 16:47:51 -0400

    demos: Add variables example

---
+++ demos/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var factor, favorite_number int
+var name string
+
+func main() {
+	var three int = 5
+
+    //But I just want to declare them...
+    magic := "whoa"
+
+	factor = 5
+
+	fmt.Println(factor)
+}
