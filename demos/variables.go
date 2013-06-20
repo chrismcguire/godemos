66e45f1a2493114d11b9eb39752e9ef1a23281c9
Chris McGuire <chris.r.mcguire@gmail.com>
2013-06-20 09:34:59 -0400

    demos.variables: Reorder declarations and comments.

--- demos/variables.go
+++ demos/variables.go
@@ -6,12 +6,13 @@ var factor, favorite_number int
 var name string
 
 func main() {
+	factor = 5
+	fmt.Println(factor)
+
 	var three int = 5
 
     //But I just want to declare them...
     magic := "whoa"
+    fmt.Println("that is " + magic)
 
-	factor = 5
-
-	fmt.Println(factor)
 }
